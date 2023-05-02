afe9c5df54c383b62f951894f3ae07e92602e56d
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-02 10:04:31 +0330

    #2 Config App

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     // './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend : {
+      fontFamily : {
+        'iranyekan-regular' : ['iranyekan-regular'],
+        'iranyekan-bold' : ['iranyekan-bold'],
+        'iranyekan-thin' : ['iranyekan-thin'],
+        'iranyekan-light' : ['iranyekan-light'],
+        'iranyekan-medium' : ['iranyekan-medium'],
+        'iranyekan-extraBold' : ['iranyekan-extraBold'],
+        'iranyekan-black' : ['iranyekan-black'],
+        'iranyekan-extraBlack' : ['iranyekan-extraBlack'],
+      }
+    }
+  },
   plugins: [],
 }
