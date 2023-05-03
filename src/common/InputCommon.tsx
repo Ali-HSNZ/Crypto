dc2ea4dcf6fa45275af22f325ea90ca439b7d450
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-03 13:37:01 +0330

    install 'date picker' and used in index register page

--- src/common/InputCommon.tsx
+++ src/common/InputCommon.tsx
@@ -8,12 +8,15 @@ type TInputCommonProps = {
      inputType : 'text' | 'password' | 'tel'
      children? : JSX.Element,
      childrenParentHover? : string
+     value? : string | number
+     disabled? : undefined | true
 }
 
-const InputCommon : FC<TInputCommonProps> = ({icon , inputType , title  , placeholder , children , childrenParentHover}) : JSX.Element => {
+const InputCommon : FC<TInputCommonProps> = ({icon , disabled , inputType , title  , placeholder , children , childrenParentHover , value}) : JSX.Element => {
      
      const inputRef = useRef<HTMLInputElement>(null)
-
+     console.log("value : ",value);
+     
      const focusHandler = () => {
           if(inputRef.current)
           inputRef.current.focus()
@@ -36,9 +39,9 @@ const InputCommon : FC<TInputCommonProps> = ({icon , inputType , title  , placeh
                {/* Title */}
                <p className='font-iranyekan-bold text-sm absolute top-[-10px] right-10 bg-white px-3'>{title}</p>
                {/* input */}
-               <input ref={inputRef} placeholder={placeholder} type={inputType}  className='font-iranyekan-regular placeholder:text-sm placeholder:text-gray-400 focus:border-gray-300 hover:border-gray-300 border-gray-200 pl-11 pr-[70px] border-2 w-full py-4 rounded-full outline-none' />
+               <input disabled={disabled ?? false} ref={inputRef}  placeholder={placeholder} type={inputType}  className='font-iranyekan-regular placeholder:text-sm placeholder:text-gray-400 focus:border-gray-300 hover:border-gray-300 border-gray-200 pl-11 pr-[70px] border-2 w-full py-4 rounded-full outline-none' />
                {/* Icon */}
-               <button onClick={focusHandler} className='absolute top-0 right-0 pr-6 px-2 bottom-0 rounded-r-full '>{icon}</button>
+               <button  onClick={focusHandler} className='absolute top-0 right-0 pr-6 px-2 bottom-0 rounded-r-full '>{icon}</button>
                {/* Line */}
                <div className='w-fit h-fit px-2 border rotate-90 absolute right-[50px] top-6'></div>
                {/* show/hidden password */}
@@ -49,14 +52,14 @@ const InputCommon : FC<TInputCommonProps> = ({icon , inputType , title  , placeh
                               <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                          </svg>
                     ) : (
-                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                         <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
                          </svg>
                     )
                }
                </button>}
                {/* children */}
-               {inputType !== 'password' && <div className='absolute top-0 left-0 pl-4 flex items-center justify-center pr-2 bottom-0 rounded-l-full'>{children}</div>}
+               {inputType !== 'password' && <div className='absolute top-0 left-0 pl-3 flex items-center justify-center pr-2 bottom-0 rounded-l-full'>{children}</div>}
           </div>
      );
 }
--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -2,8 +2,21 @@ import logo from '@/images/Logo.png'
 import Link from "next/link";
 import InputCommon from "@/common/InputCommon";
 
+//? Date Picker =>
+import DatePicker,{DateObject} from "react-multi-date-picker"
+import type{Value} from "react-multi-date-picker"
+import Icon from "react-multi-date-picker/components/icon"
+import persian from "react-date-object/calendars/persian"
+import persian_fa from "react-date-object/locales/persian_fa"
+import "react-multi-date-picker/styles/layouts/mobile.css"
+import {useState} from 'react'
 
 const RegisterPage = () => {
+
+     const [birthday, setBirthday] = useState<Value>(" ")
+
+     const weekDays = ["ش", "ی", "د", "س", "چ", "پ", "ج"]
+
 
      return (  
           <section className="w-full h-screen xl:p-8 bg-transparent flex flex-col md:flex-row">
@@ -72,7 +85,33 @@ const RegisterPage = () => {
                               inputType="tel"
                               title="تاریخ تولد"
                               placeholder="۱۳۷۰/۰۶/۳۱"
+                              value={birthday?.toString()}
+                              disabled
+                              children={
+                                   <DatePicker
+                                        value={birthday}
+                                        onChange={setBirthday}
+                                        buttons={true}
+                                        render={(value, openCalendar) => {
+                                             return (
+                                               <button onClick={openCalendar} className=" p-2 bg-blue-100 rounded-full">
+                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-600">
+                                                       <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                                                  </svg>
+                                               </button>
+                                             )
+                                        }}     
+                                        calendar={persian}
+                                        weekDays={weekDays}
+                                        locale={persian_fa}
+                                        inputClass=""
+                                        className="rmdp-mobile font-iranyekan-regular"
+                                   />
+                              }
                          />
+                         {/* <div className='relative border-2'>
+                              
+                         </div> */}
                     </section>
                     <hr className="w-full border-gray-300"/>
                     <section className="w-full pb-6 flex justify-end pl-6">
