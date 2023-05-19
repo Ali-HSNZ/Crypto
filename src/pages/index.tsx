e5afb6cdeba60c8bfabc456f964d41e5660ccbef
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-20 02:14:01 +0330

    use try-catch in getServerSideProps and showing 429 error when Fetching data from the server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,12 @@ import Head from 'next/head'
 
 
 const Home = (
-  { priceHistories, weekTransactions, favoriteCoins }: {
+  { priceHistories, weekTransactions, favoriteCoins, serverError }: {
 
     priceHistories: Array<TCrypto_priceHistoriesRes>,
     weekTransactions: TCrypto_weekTransactions,
     favoriteCoins: Array<TCrypto_favoriteCoinsRes>,
+    serverError: string
   }
 ) => {
 
@@ -52,6 +53,10 @@ const Home = (
         )}
 
         <div className={`${isOpenNavbar && isLgScreen ? "col-span-8" : "col-span-10"}  `}>
+
+          {/* 429 error when Fetching data from the server */}
+          <p className='text-red-600 font-iranyekan-bold pb-6'>{serverError}</p>
+
           <Header setIsOpenNavbar={setIsOpenNavbar} isOpenNavbar={isOpenNavbar} />
 
           {/* Price Histories Chart */}
@@ -89,84 +94,93 @@ export const getServerSideProps = async () => {
   // Base URL for CoinGecko API
   const baseUrl = "https://api.coingecko.com/api/v3";
 
-  // Fetching Week Transactions Data
-  const weekTransactions = await axios.get(`${baseUrl}/coins/bitcoin/market_chart?vs_currency=usd&days=7`)
+  try {
 
-  // Fetching Favorite Coins Data
-  const favoriteCoins = await axios.get(`${baseUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h`)
+    const weekTransactions = await axios.get(`${baseUrl}/coins/bitcoin/market_chart?vs_currency=usd&days=7`)
 
-  // Fetching PriceChanges Data
-  const fetchCoins = async () => {
+    // Fetching Favorite Coins Data
+    const favoriteCoins = await axios.get(`${baseUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h`)
 
-    const priceUrl = `${baseUrl}/simple/price?ids=ripple,ethereum,bitcoin,dogecoin,tether&vs_currencies=usd&include_24hr_change=true`;
+    // Fetching PriceChanges Data
+    const fetchCoins = async () => {
 
-    const fetchCoinData = (coinId: string) => axios.get(`${baseUrl}/coins/${coinId}/market_chart?vs_currency=usd&days=365`);
+      const priceUrl = `${baseUrl}/simple/price?ids=ripple,ethereum,bitcoin,dogecoin,tether&vs_currencies=usd&include_24hr_change=true`;
 
-    const [rippleRes, ethereumRes, bitcoinRes, dogecoinRes, tetherRes, priceRes] = await axios.all([
-      fetchCoinData("ripple"),
-      fetchCoinData("ethereum"),
-      fetchCoinData("bitcoin"),
-      fetchCoinData("dogecoin"),
-      fetchCoinData("tether"),
-      axios.get(priceUrl)
-    ]);
+      const fetchCoinData = (coinId: string) => axios.get(`${baseUrl}/coins/${coinId}/market_chart?vs_currency=usd&days=365`);
 
-    return [
-      {
-        history: rippleRes.data.prices.slice(0, 10),
-        usd: priceRes.data.ripple.usd,
-        usd_24h_change: priceRes.data.ripple.usd_24h_change,
-        symbol: "XRP",
-        fa_name: "ریپل",
-        name: "ripple",
-        imageUrl: "https://cdn.iconscout.com/icon/free/png-512/free-ripple-13-646080.png?f=avif&w=256"
-      },
-      {
-        history: bitcoinRes.data.prices.slice(0, 10),
-        usd: priceRes.data.bitcoin.usd,
-        usd_24h_change: priceRes.data.bitcoin.usd_24h_change,
-        symbol: "BTC",
-        fa_name: "بیتکوین",
-        name: "bitcoin",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg"
-      },
-      {
-        history: dogecoinRes.data.prices.slice(0, 10),
-        usd: priceRes.data.dogecoin.usd,
-        usd_24h_change: priceRes.data.dogecoin.usd_24h_change,
-        symbol: "DOGE",
-        fa_name: "دوج‌کوین",
-        name: "dogecoin",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/fa/d/d0/Dogecoin_Logo.png"
-      },
-      {
-        history: tetherRes.data.prices.slice(0, 10),
-        usd: priceRes.data.tether.usd,
-        usd_24h_change: priceRes.data.tether.usd_24h_change,
-        symbol: "USDT",
-        fa_name: "تتر",
-        name: "tether",
-        imageUrl: "https://images.revain.org/blob/tether_logo_67a1be67_6568_475e_ac29_8790e2d210ad_84d8fedf45@128x128.png.webp"
-      },
-      {
-        history: ethereumRes.data.prices.slice(0, 10),
-        usd: priceRes.data.ethereum.usd,
-        usd_24h_change: priceRes.data.ethereum.usd_24h_change,
-        symbol: "ETH",
-        fa_name: "اتریوم",
-        name: "ethereum",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Eth-diamond-rainbow.png"
+      const [rippleRes, ethereumRes, bitcoinRes, dogecoinRes, tetherRes, priceRes] = await axios.all([
+        fetchCoinData("ripple"),
+        fetchCoinData("ethereum"),
+        fetchCoinData("bitcoin"),
+        fetchCoinData("dogecoin"),
+        fetchCoinData("tether"),
+        axios.get(priceUrl)
+      ]);
+
+      return [
+        {
+          history: rippleRes.data.prices.slice(0, 10),
+          usd: priceRes.data.ripple.usd,
+          usd_24h_change: priceRes.data.ripple.usd_24h_change,
+          symbol: "XRP",
+          fa_name: "ریپل",
+          name: "ripple",
+          imageUrl: "https://cdn.iconscout.com/icon/free/png-512/free-ripple-13-646080.png?f=avif&w=256"
+        },
+        {
+          history: bitcoinRes.data.prices.slice(0, 10),
+          usd: priceRes.data.bitcoin.usd,
+          usd_24h_change: priceRes.data.bitcoin.usd_24h_change,
+          symbol: "BTC",
+          fa_name: "بیتکوین",
+          name: "bitcoin",
+          imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg"
+        },
+        {
+          history: dogecoinRes.data.prices.slice(0, 10),
+          usd: priceRes.data.dogecoin.usd,
+          usd_24h_change: priceRes.data.dogecoin.usd_24h_change,
+          symbol: "DOGE",
+          fa_name: "دوج‌کوین",
+          name: "dogecoin",
+          imageUrl: "https://upload.wikimedia.org/wikipedia/fa/d/d0/Dogecoin_Logo.png"
+        },
+        {
+          history: tetherRes.data.prices.slice(0, 10),
+          usd: priceRes.data.tether.usd,
+          usd_24h_change: priceRes.data.tether.usd_24h_change,
+          symbol: "USDT",
+          fa_name: "تتر",
+          name: "tether",
+          imageUrl: "https://images.revain.org/blob/tether_logo_67a1be67_6568_475e_ac29_8790e2d210ad_84d8fedf45@128x128.png.webp"
+        },
+        {
+          history: ethereumRes.data.prices.slice(0, 10),
+          usd: priceRes.data.ethereum.usd,
+          usd_24h_change: priceRes.data.ethereum.usd_24h_change,
+          symbol: "ETH",
+          fa_name: "اتریوم",
+          name: "ethereum",
+          imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Eth-diamond-rainbow.png"
+        }
+      ];
+    };
+    const priceHistories = await fetchCoins();
+
+
+    return {
+      props: {
+        priceHistories: priceHistories,
+        weekTransactions: weekTransactions.data.prices.slice(70, 80),
+        favoriteCoins: favoriteCoins.data.sort((a: TCrypto_favoriteCoinsRes, b: TCrypto_favoriteCoinsRes) => b.circulating_supply - a.circulating_supply),
       }
-    ];
-  };
-  const priceHistories = await fetchCoins();
+    }
+  } catch (error) {
 
-
-  return {
-    props: {
-      priceHistories: priceHistories,
-      weekTransactions: weekTransactions.data.prices.slice(70, 80),
-      favoriteCoins: favoriteCoins.data.sort((a: TCrypto_favoriteCoinsRes, b: TCrypto_favoriteCoinsRes) => b.circulating_supply - a.circulating_supply),
+    return {
+      props: {
+        serverError: "خطای 429 | Too Many Requests"
+      }
     }
   }
 };
