ccf2d6b648f1cd2b9a9d324cdb6304c05b2a3cd3
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-19 23:18:56 +0330

    Moved redux store from 'src/redux/store/' to 'src/redux/'

---
+++ src/redux/store.ts
@@ -0,0 +1,20 @@
+import { ThunkAction, configureStore, Action } from "@reduxjs/toolkit";
+
+import { registerReducer } from "./slices/register";
+import wallet_assetsReducer from "./slices/wallet_assets";
+
+
+const store = configureStore({
+  reducer: {
+    register : registerReducer,
+    wallet_assets : wallet_assetsReducer,
+  },
+});
+
+export default store
+
+
+export type TRootState = ReturnType<typeof store.getState>;
+export type TAppDispatch = typeof store.dispatch
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, TRootState, unknown, Action>
