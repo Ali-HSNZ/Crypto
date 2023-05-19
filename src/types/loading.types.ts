084a05f1a02033af4bf5199a9f9c3389bd0c3330
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-19 21:16:46 +0330

    Deleted unUsed components/files

--- src/types/loading.types.ts
+++
@@ -1,6 +0,0 @@
-export type LTypes = "blank" | "balls" | "bars" | "bubbles" | "cubes" | "cylon" | "spin" | "spinningBubbles" | "spokes";
-export type TLoadingProps = {
-     type : LTypes,
-     color : string,
-     scale : number
-}
