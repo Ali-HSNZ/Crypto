63f52695fddb5a52a19a3f650f77db700466fa4f
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-19 23:46:58 +0330

    Change crypto types in => src/types/crypto.types.ts

--- src/types/crypto.types.ts
+++ src/types/crypto.types.ts
@@ -1,67 +1,33 @@
-
-//? FavoriteCoins
-export type TFavoriteCoinsResponse = {
-     image : string,
-     name : string,
-     price_change_24h : number,
-     symbol : string,
-     circulating_supply : number
+// FavoriteCoins
+export type TCrypto_favoriteCoinsRes = {
+     image: string,
+     name: string,
+     price_change_24h: number,
+     symbol: string,
+     circulating_supply: number
 }
-export interface ICrypto_favoriteCoins {
-     loading: boolean;
-     error: string | null;
-     data: null | Array<TFavoriteCoinsResponse>
-}
-
-
-//? PriceChanges
-export type TPriceChangesResponse = {
-     usd: number ,
+// PriceChanges
+export type TCrypto_priceHistoriesRes = {
+     usd: number,
      usd_24h_change: number,
      history: Array<Array<number>>
      name: string,
      fa_name: string
+     symbol : string,
+     imageUrl : string
 }
-export interface ICrypto_priceChanges {
+// walletAssets
+export type TCrypto_walletAssetsResponse = {
+     id: number
+     name: string
+     symbol: string
+     balance: number
+}
+export type ICrypto_walletAssets = {
      loading: boolean;
      error: string | null;
-     bitcoin: null | TPriceChangesResponse;
-     dogecoin: null | TPriceChangesResponse;
-     tether: null | TPriceChangesResponse;
-     ethereum: null | TPriceChangesResponse
-     ripple: null | TPriceChangesResponse
+     data: Array<TCrypto_walletAssetsResponse> | null
 }
 
-
-//? AccessWallet
-export type TCrypto_accessWalletResponse = {
-     id : number
-     name : string
-     symbol : string
-     balance : number
-}
-export interface ICrypto_accessWallet {
-     loading: boolean;
-     error: string | null;
-     data: Array<TCrypto_accessWalletResponse> | null
-}
-
-
-//? Trading View
-export type TCrypto_tradingViewResponse = {
-     market_caps : Array<Array<number>>
-     prices : Array<Array<number>>
-     total_volumes : Array<Array<number>>
-}
-export interface ICrypto_tradingView {
-     loading: boolean;
-     error: string | null;
-     data: TCrypto_tradingViewResponse | null
-}
-
-//? lastWeekTransactions
-export interface ICrypto_weekTransactions {
-     loading: boolean;
-     error: string | null;
-     data: Array<Array<number>> | null
-}
+// WeekTransactions
+export type TCrypto_weekTransactions = Array<Array<number>> | null
