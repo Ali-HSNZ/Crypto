21ece39c499741ce0f8212715a46ac6093c03ab8
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-20 13:58:22 +0330

    Create 'other.types.ts' in  src/types/

---
+++ src/types/other.types.ts
@@ -0,0 +1,30 @@
+import { TCity, TProvince } from "./register.types"
+
+export type TSelectboxProps = {
+     setSelected: TProvince | TCity | any,
+     selected: string | any,
+     query: string,
+     notFoundTitle: string,
+     setQuery: React.Dispatch<React.SetStateAction<any>>,
+     filteredData: TProvince | TCity | any,
+     icon: JSX.Element
+     title: string
+     name: string
+     formik: any,
+     // optional =>
+     isDisabled?: boolean,
+     placeholder?: string,
+     disabled?: boolean
+}
+export type TInputCommonProps = {
+     title: string,
+     icon: JSX.Element,
+     placeholder?: string,
+     type: 'password' | 'tel' | 'text' | "email"
+     children?: JSX.Element,
+     dir?: 'rtl' | 'ltr',
+     formik?: any
+     name?: string | any
+     maxLength?: number | undefined;
+     disabled?: boolean | undefined
+}
