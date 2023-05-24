6dfc9be2d848b340d68fcdf9338b0904a9fbd793
Ali Hassanzadeh <aliatraby@gmail.com>
2023-05-24 12:40:44 +0330

    delete 'toPersianDigits' file in utils folder => src/utils/toPersianDigits.ts

--- src/utils/toPersianDigits.ts
+++
@@ -1,4 +0,0 @@
-export const toPersianDigits = (requestDigits : string | number) => {
-	const persianDigits = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹']
-	return requestDigits !== undefined ? requestDigits.toString().replace(/\d/g , (e) => persianDigits[parseInt(e)]) : ""
-}
